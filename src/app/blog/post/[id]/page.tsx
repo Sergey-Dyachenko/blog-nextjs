67d58e2a69ce25fed8bf13d557da1aef5f7e2a3e
Serhii Diachenko <s.diachenko@steel.mil.ua>
2025-02-06 18:10:24 +0200

    fix build

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -3,6 +3,10 @@ import Post from '@/app/ui/components/posts/Post';
 
 export default function Page({params} : {params: {id: string}}) {
   const post = posts.find((post) => post.id === params.id);
+
+  if (!post || !post.id || !post.title || !post.content || !post.date) {
+    return <div>Post not found or missing required fields</div>;
+  }
   return (
     <>
       <h1 className="text-purple-800">Post</h1>
